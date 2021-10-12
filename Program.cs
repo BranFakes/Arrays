d5772dc20c73e17220690a367170768213c678eb
Brandon VanBuskirk <Brandonvb@hotmail.ca>
2021-10-12 09:37:55 -0300

    tested

--- Program.cs
+++ Program.cs
@@ -10,6 +10,45 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Ammo System");
+            //WE WISH TO STORE THE AMMO OF ALL WEAPONS
+            //pistol + 6/6
+            //shotgun = 2/2
+
+
+            int Weapon = 0; // 0 = PISTOL, 1 = SHOTGUN, ETC.
+
+            Console.WriteLine("BEFORE Arrays");
+            int pistolAmmo = 6; // declaration + initialization
+            int shotGunAmmo = 2;
+            int bfgAmmo = 20;
+
+            // HOW DO WE SHOW THE AMMO OF THE CURRENT WEAPON?
+
+            if (Weapon == 0)
+            {
+                 Console.WriteLine("Pistol Ammo: " + pistolAmmo);
+            }
+
+           else if (Weapon == 1)
+            {
+                Console.WriteLine("Shotgun Ammo: " + shotGunAmmo);
+            }
+
+           else if (Weapon == 2)
+            {
+                Console.WriteLine("bfg Ammo: " + bfgAmmo);
+            }
+
+
+
+
+            Console.WriteLine("Arrays:");
+
+
+
+
+            Console.ReadKey(true);
         }
     }
 }
